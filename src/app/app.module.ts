1497fc2bdb40c289fc8eafa5d39e40313ae75621
Victoria Retallick <vretallick@zedge.net>
2018-04-18 21:51:49 +0200

    Add task components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TaskManagerComponent } from './task-manager/task-manager.component';
+import { TaskComponent } from './task-manager/task/task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskManagerComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule
